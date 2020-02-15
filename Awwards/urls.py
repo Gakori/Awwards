b0a785d88bb6efc36052922862c5e98768e6fad1
faith gakori <faithgakori506@gmail.com>
2020-02-15 22:14:47 +0300

    add profile route

--- Awwards/urls.py
+++ Awwards/urls.py
@@ -27,9 +27,8 @@ from django.conf.urls.static import static
 
 from django.conf import settings
 
-
-
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     
     path('wards/', include('wards.urls')),
@@ -41,4 +40,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
 
     path('logout/', auth_views.LogoutView.as_view(template_name='auth/logout.html'), name='logout'), 
+    
+    path('profile/', wards_views.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
