e5c740303f5fee7947c20e592b3c9043e8a18d92
faith gakori <faithgakori506@gmail.com>
2020-02-15 11:47:09 +0300

    add views for register

--- wards/views.py
+++ wards/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import UserRegistrationForm
+
+from django.http import HttpResponse
+
+from django.contrib import messages
+
+from django.contrib.auth.forms import UserCreationForm
+
+def home(request):
+    return HttpResponse('<h2>oiuytr</h2>')
+
+def register(request):
+    '''
+    function that renders the renders the registration form
+    '''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account has  been created.You are now able to login')#flash message
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth/register.html', {'form': form})
+        
