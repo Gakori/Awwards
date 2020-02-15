10c45e83d259cb84fa445f0f0be4e41b5878de31
faith gakori <faithgakori506@gmail.com>
2020-02-15 22:16:25 +0300

    add fields to profile and user form

--- wards/forms.py
+++ wards/forms.py
@@ -4,9 +4,24 @@ from django.contrib.auth.models import User
 
 from django import forms
 
+from .models import Profile
+
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
     
     class Meta:
         models = 'User'
         fields = ['Username', 'Email', 'Password1', 'Password2']
+        
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        
+class ProfileUpdateForm(forms.ModelForm):
+    
+    class Meta:
+        model = Profile
+        fields = ['image' ,'bio', 'contacts']
