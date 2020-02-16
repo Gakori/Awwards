27cf8dd298a0f4e75151559e072db2b88f74cea6
faith gakori <faithgakori506@gmail.com>
2020-02-16 21:07:40 +0300

    add review class

--- wards/models.py
+++ wards/models.py
@@ -23,4 +23,16 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
     
+class Review(models.Model):
+    body = models.CharField(max_length=200)
+    date_posted = models.DateField(default=timezone.now)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_id=models.ForeignKey(Post, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.body
+    
+    
+    
+    
             
