393edcbe6b9984af14ad98c33a147a0bcaaf6041
faith gakori <faithgakori506@gmail.com>
2020-02-16 21:06:26 +0300

    add review views

--- wards/views.py
+++ wards/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm, PostForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, PostForm, ReviewForm
 
 from django.http import HttpResponse
 
@@ -10,11 +10,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.decorators import login_required
 
-from django.views.generic import ListView , CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-from .models import Post
+from .models import Post, Review
 
 from django.urls import reverse_lazy
 
@@ -35,35 +35,63 @@ def home(request):
         posts = Post.objects.all()
     except Post.DoesNotExist:
         posts = None
-    
+        
     return render(request, 'wards/home.html', { 'posts': posts, 'form': form })
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'author', 'description', 'live_link']
+    fields = ['title', 'author', 'description', 'live_link', 'photo']
     success_url = reverse_lazy('wards-home')
     
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+class PostDetailView(DetailView):
+    model = Post
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title']
+    success_url = reverse_lazy('wards-home')
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def register(request):
-    '''
-    function that renders the renders the registration form
-    '''
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account has  been created.You are now able to login')#flash message
-            return redirect('login')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('wards-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'auth/register.html', {'form': form})
         
 @login_required
 def profile(request):
+    '''
+    function that displays current user
+    '''
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
@@ -73,9 +101,9 @@ def profile(request):
             p_form.save()
             messages.success(request, f'Your account has been updated')
             return redirect('profile')
-        else:
-            u_form = UserUpdateForm(instance=request.user)
-            p_form = ProfileUpdateForm(instance=request.user.profile)
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
     
     context = {
         'u_form': u_form,
@@ -83,4 +111,21 @@ def profile(request):
     }
     
     return render(request, 'wards/profile.html', context)
+
+def review(request):
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.save()
+    else:
+        form = ReviewForm()
+
+    try:
+        reviews = Review.objects.all()
+    except Review.DoesNotExist:
+        reviews = None
         
+    return render(request, 'wards/review.html', { 'reviews': reviews, 'form': form })
+    
+    
